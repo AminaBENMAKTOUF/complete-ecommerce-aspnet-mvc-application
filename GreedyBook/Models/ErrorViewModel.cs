19d774e08ceb8c5012a18811e6309d170e830ad9
USER <USER@BEST-TECHNOLOGY>
2023-06-13 16:41:59 +0100

    Ajoutez des fichiers projet.

---
+++ GreedyBook/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GreedyBook.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
